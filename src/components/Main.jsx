36be1a77386812bfbbf6ab947287c315fe766f3c
Sakhi Rotliwala <rotliwalasakhi@gmail.com>
2024-01-12 13:16:36 +0530

    home screen updated

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,16 +2,42 @@ import React from "react";
 
 function Main() {
   return (
-    <div className="w-full h-full" style={{ backgroundColor: "#f8f7f4" }}>
-      <div className="flex justify-center mt-14">
-        <button className="flex items-center bg-[#ffda79] rounded-3xl px-7 h-10 animate-pulse-reduce font-[customFont] text-[18px] font-medium">
-          Over 3 million ready-to-work creatives!
-        </button>
+    <section
+      className="pt-20 text-center w-full h-44"
+      style={{ backgroundColor: "#f8f7f4" }}
+    >
+      {/* section for first page */}
+      <div className="flex px-8 box-content flex-col items-center m-auto overflow-hidden">
+        {/*home container */}
+        <div className="flex items-center">
+          <button className=" bg-[#ffda79] rounded-3xl px-7 h-10 animate-pulse-reduce font-[customFont] text-[18px] font-medium">
+            Over 3 million ready-to-work creatives!
+          </button>
+        </div>
+        <h1 className="flex justify-center items-center text-[72px] font-[font2] mt-10 mb-6 mx-auto leading-[76px] w-[768px]">
+          The world's destination for design
+        </h1>
+        <div className="text-xl font-normal items-center">
+          Get inspired by the work of millions of top-rated designers & agencies
+          around the world.
+        </div>
+        <div className="flex justify-center items-center">
+          <button className="bg-black text-white px-6 h-12 rounded-3xl hover:bg-slate-700 mt-8">
+            Get started
+          </button>
+        </div>
       </div>
-      <h1 className="flex justify-center items-center text-[72px] font-[font2] mt-12">
-        The world's destination for design
-      </h1>
-    </div>
+      <div class="overflow-x-hidden">
+        <div class="py-12 animate-marquee whitespace-nowrap columns-24 ">
+          <span class="mx-4 text-4xl">Marquee Item 1</span>
+          <span class="mx-4 text-4xl">Marquee Item 2</span>
+          <span class="mx-4 text-4xl">Marquee Item 3</span>
+          <span class="mx-4 text-4xl">Marquee Item 4</span>
+          <span class="mx-4 text-4xl">Marquee Item 5</span>
+        </div>
+      </div>
+      {/* Marquee cards */}
+    </section>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ export default {
       animation: {
         "pulse-reduce":
           "pulse-reduce 32s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+        marquee: "marquee 25s linear infinite",
       },
       keyframes: {
         "pulse-reduce": {
@@ -18,6 +19,10 @@ export default {
           "50%": { backgroundColor: "#d2bcf3", opacity: "1" },
           "75%": { backgroundColor: "#edf3d8", opacity: "1" },
           "100%": { backgroundColor: "#ffda79", opacity: "1" },
+        },
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
         },
       },
     },
