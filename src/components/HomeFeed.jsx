b9338cc00a502b39994b07d330f35d33005c1008
Sakhi Rotliwala <rotliwalasakhi@gmail.com>
2024-01-13 19:12:31 +0530

    footers added

--- src/components/HomeFeed.jsx
+++ src/components/HomeFeed.jsx
@@ -1,12 +1,25 @@
 import React from "react";
+import YellowFooter from "./YellowFooter";
 
 function HomeFeed() {
   return (
     <section className="">
       <h2 className="my-[43px] mx-[0] text-6xl">Explore inspiring designs</h2>
-      <div className="">
-        <button className="mt-20 border-gray-950 rounded-2xl">Browse more inspiration</button>
+      <div className="grid gap-[32px] grid-cols-4 grid-rows-4 px-8">
+        <div className="flex items-end opacity-100 w-80 h-64 rounded-3xl shadow-xl">
+          <div className="grid grid-flow-row items-center gap-2 leading-3 z-[2] absolute opacity-100 pt-4 pb-4 px-5">
+            <div className="flex align-baseline font-semibold bg-transparent ">
+              name
+            </div>
+          </div>
+        </div>
       </div>
+      <div className="">
+        <button className="border-solid border-2 border-gray-950 px-6 h-12 rounded-3xl">
+          Browse more inspiration
+        </button>
+      </div>
+      <YellowFooter/>
     </section>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -33,14 +33,14 @@ function Main() {
         <div class="grid gap-[32px] overflow-hidden relative z-[2] mt-auto mb-auto mx-0 pt-[72px] pb-[72px] px-auto bg-transparent">
           <div className="animate-marquee">
             <div className="grid gap-[32px] grid-flow-col w-fit">
-              <div className=" bg-profile-card opacity-100 w-[273px] h-[340px] rounded-3xl shadow-xl">
+              <div className="flex items-end bg-profile-card opacity-100 w-[273px] h-[340px] rounded-3xl shadow-xl">
                 <div className="grid grid-flow-row gap-2 text-[30px] leading-3 z-[2] absolute opacity-100 pt-4 pb-4 px-5 ">
                   <div className="flex w-[100%] text-[100%] align-baseline text-white font-medium bg-transparent">
                     name
                   </div>
                 </div>
               </div>
-              <div className="bg-profile-card opacity-100 w-[273px] h-[340px] rounded-3xl shadow-xl">
+              <div className="flex items-end bg-profile-card opacity-100 w-[273px] h-[340px] rounded-3xl shadow-xl">
                 <div className="grid grid-flow-row items-end gap-2 text-[30px] leading-3 z-[2] absolute opacity-100 pt-4 pb-4 px-5 ">
                   <div className="flex w-[100%] text-[100%] align-baseline text-white font-semibold bg-transparent">
                     name
